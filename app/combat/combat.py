c2e45c70d2446b9f317f680eb51613ae4bb0ecb4
Charlie Ensor <admin@charlieensor.com>
2023-05-07 07:18:57 +0100

    popup in a class

--- app/combat/combat.py
+++ app/combat/combat.py
@@ -1,6 +1,7 @@
 import pygame, random
 from pygame.locals import *
 from app.engine.components.button import Button
+from app.engine.components.popup import Popup
 from app.engine.constants import *
 from app.combat.card import Card
 from app.combat.cursed_card import CursedCard
@@ -196,16 +197,14 @@ class Combat:
         pygame.display.flip()
 
     def popup(self, text):
-        popup_width = 300
-        popup_height = 100
-        popup_x = self.game.config.get_width() // 2 - popup_width // 2
-        popup_y = self.game.config.get_height() // 2 - popup_height // 2
-        popup_rect = pygame.Rect(popup_x, popup_y, popup_width, popup_height)
-        pygame.draw.rect(self.game.screen, (255, 255, 255), popup_rect)
-        font = pygame.font.Font('app\\assets\\fonts\\cursedFont.tff', 24)
-        text_surface = font.render(text, True, (0, 0, 0))
-        text_rect = text_surface.get_rect(center=(popup_x + popup_width // 2, popup_y + popup_height // 2))
-        self.game.screen.blit(text_surface, text_rect)
+        popup = Popup(
+            self.game.config.get_width() // 2, 
+            self.game.config.get_height() // 2, 
+            text,
+            width=300,
+            height=100
+        )
+        popup.draw(self.game.screen)
         pygame.display.flip()
         pygame.time.wait(500)
         pygame.display.flip()

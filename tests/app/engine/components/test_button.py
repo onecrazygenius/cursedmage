968ee1879eac99da85171ab8be672e6bb1686ef3
James Hillyard <james.hillyard@payara.fish>
2023-05-07 01:01:23 +0100

    Engine Package Unit Tests
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

---
+++ tests/app/engine/components/test_button.py
@@ -0,0 +1,49 @@
+import unittest
+from unittest.mock import MagicMock, patch
+import pygame
+from app.engine.components.button import Button
+
+
+class TestButton(unittest.TestCase):
+    def setUp(self):
+        self.text = "Test Button"
+        self.x = 100
+        self.y = 100
+        self.callback = MagicMock()
+        self.width = 200
+        self.height = 50
+        self.font_size = 24
+
+        self.button = Button(self.text, self.x, self.y, self.callback, self.width, self.height, self.font_size)
+
+    def test_init(self):
+        self.assertEqual(self.button.text, self.text)
+        self.assertEqual(self.button.x, self.x)
+        self.assertEqual(self.button.y, self.y)
+        self.assertEqual(self.button.callback, self.callback)
+        self.assertEqual(self.button.width, self.width)
+        self.assertEqual(self.button.height, self.height)
+        self.assertEqual(self.button.font_size, self.font_size)
+        self.assertIsInstance(self.button.rect, pygame.Rect)
+
+    @patch("app.engine.components.button.pygame.draw.rect")
+    @patch("app.engine.components.button.pygame.font.Font")
+    @patch("app.engine.components.button.pygame.Surface")
+    def test_draw(self, surface_mock, font_mock, rect_mock):
+        screen_mock = MagicMock()
+        font_instance_mock = font_mock.return_value
+        text_surface_mock = font_instance_mock.render.return_value
+
+        self.button.draw(screen_mock)
+
+        rect_mock.assert_called_once_with(screen_mock, (0, 0, 0), self.button.rect)
+        font_mock.assert_called_once_with(None, self.font_size)
+        font_instance_mock.render.assert_called_once_with(self.text, True, (255, 255, 255))
+        text_surface_mock.get_rect.assert_called_once()
+
+    # It's very difficult to test the button being clicked as an independent unit test without PyGame.
+    # A more effective test can be implemented using PyGame.
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/app/engine/components/test_slider.py
@@ -0,0 +1,57 @@
+import unittest
+from unittest.mock import MagicMock
+import pygame
+
+from app.engine.components.slider import Slider
+
+
+class TestSlider(unittest.TestCase):
+
+    def setUp(self):
+        pygame.init()
+        self.callback = MagicMock()
+        self.slider = Slider(10, 20, 100, 50, 200, self.callback)
+
+    def test_init(self):
+        self.assertEqual(self.slider.x, 10)
+        self.assertEqual(self.slider.y, 20)
+        self.assertEqual(self.slider.width, 100)
+        self.assertEqual(self.slider.value, 50)
+        self.assertEqual(self.slider.max_value, 200)
+        self.assertEqual(self.slider.callback, self.callback)
+
+    def test_is_on_handle(self):
+        # This fails because 10 < 10 is not true. I believe this is an error in the is_on_handle method
+        self.assertTrue(self.slider._is_on_handle((25, 25)))
+        self.assertFalse(self.slider._is_on_handle((100, 100)))
+
+    # Various aspects of this test fail because of the error identified in the test above. Needs fixing then reviewing
+    def test_handle_event(self):
+        # Test MOUSEBUTTONDOWN
+        mouse_down_event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, pos=(25, 25))
+        self.slider.handle_event(mouse_down_event)
+        self.assertTrue(self.slider.dragging)
+
+        mouse_down_event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, pos=(30, 30))
+        self.slider.handle_event(mouse_down_event)
+        self.assertTrue(self.slider.dragging)
+
+        # Test MOUSEBUTTONUP
+        mouse_up_event = pygame.event.Event(pygame.MOUSEBUTTONUP)
+        self.slider.handle_event(mouse_up_event)
+        self.assertFalse(self.slider.dragging)
+
+        # Test MOUSEMOTION
+        # To test mouse motion there will always have been a button down event too
+        mouse_down_event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, pos=(25, 25))
+        self.slider.handle_event(mouse_down_event)
+        mouse_motion_event = pygame.event.Event(pygame.MOUSEMOTION, pos=(40, 25))
+        self.slider.handle_event(mouse_motion_event)
+        self.assertTrue(self.slider.dragging)
+        self.assertEqual(self.slider.handle_x, 40)
+        self.assertEqual(self.slider.value, 60)
+        self.callback.assert_called_with(60)
+
+
+if __name__ == '__main__':
+    unittest.main()

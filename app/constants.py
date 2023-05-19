6c547cb555f088590a96ea3da829ef1f4d2f09f9
Charlie Ensor <admin@charlieensor.com>
2023-05-19 21:16:43 +0100

    fixed constants

--- app/constants.py
+++ app/constants.py
@@ -1,25 +1,16 @@
+# Screen size
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-# Dungeon size
+# TEMP Dungeon size
 DUNGEON_SIZE_X = 5
 DUNGEON_SIZE_Y = 5
 
-# Screen size
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
 # Timers
 STATE_WAIT_TIME=500
-
-# Sizings
-HEALTH_BAR_WIDTH = 200
-HEALTH_BAR_HEIGHT = 20
-PLAYER_HEALTH_COLOR = (0, 255, 0)  # Green color for player health bar
-ENEMY_HEALTH_COLOR = (255, 0, 0)  # Red color for enemy health bar
-CARD_WIDTH = 100
-CARD_HEIGHT = 150
-CARD_START_X = 50
-CARD_START_Y = 400
-CARD_GAP = 20

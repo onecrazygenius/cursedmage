213b9327e85fb58aeb6d61d2963ea19d5b212893
James Hillyard <james.hillyard@payara.fish>
2023-06-06 15:55:49 +0100

    Fix Path Error for Exe vs Local Use
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

--- app/constants.py
+++ app/constants.py
@@ -38,4 +38,4 @@ def resource_path(relative_path):
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+    return os.path.join(base_path, relative_path.lstrip('/'))

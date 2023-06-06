c7559f86bd028a40b47566e3771e630c5caaa31c
Charlie Ensor <admin@charlieensor.com>
2023-06-06 21:24:39 +0100

    initial website commit

---
+++ client/src/components/Blog.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react'
+
+import Popup from './Popup'
+
+const Blog = ({props}) => {
+
+    const [openPopup, setOpenPopup] = useState(false);
+
+    const { title, content, image } = props;
+    const preview = content.substring(0, 100) + "...";
+
+    const toggle = () => {
+        setOpenPopup(!openPopup);
+    }
+
+    return ( 
+    <div class="blog">
+        <a onClick={toggle}>
+            <img src="images/{image}" />
+        </a>
+        <div class="bio">
+            <h2>{title}</h2>
+            <a onClick={toggle} class="button">Read</a>
+            <p>{preview}</p>
+        </div>
+
+        {openPopup && <Popup props={props} setOpenPopup={setOpenPopup} />}
+    </div> 
+    );
+}
+ 
+export default Blog;

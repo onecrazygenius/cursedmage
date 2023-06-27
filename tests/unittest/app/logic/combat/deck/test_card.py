78bc7034ec283ed7623cf488e65214424494fca7
James Hillyard <james.hillyard@payara.fish>
2023-06-27 14:36:32 +0100

    Add Test Structure
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

---
+++ tests/unittest/app/logic/combat/deck/test_card.py
@@ -0,0 +1,20 @@
+import unittest
+
+from app.logic.combat.deck.card import Card
+
+
+class TestCard(unittest.TestCase):
+    def test_valid_card_initialisation(self):
+        card = Card("Fireball")
+        self.assertEqual(card.name, "Fireball")
+        self.assertEqual(card.card_type, "attack")
+        self.assertEqual(card.power, 20)
+        self.assertEqual(card.cost, 1)
+        self.assertFalse(card.cursed)
+        self.assertIsNotNone(card.image)
+        self.assertIsNone(card.target)
+        self.assertEqual(card.position, (0, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()

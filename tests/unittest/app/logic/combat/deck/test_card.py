31528317c280a414c4a486813040c5e7ca591237
iFateAssassin <33941920+iFateAssassin@users.noreply.github.com>
2023-06-29 04:05:37 +0100

    Added Tests for card enemy and character

--- tests/unittest/app/logic/combat/deck/test_card.py
+++ tests/unittest/app/logic/combat/deck/test_card.py
@@ -16,7 +16,25 @@ class TestCard(unittest.TestCase):
         self.assertEqual(card.position, (0, 0))
 
     def test_invalid_card_initialisation(self):
-        self.assertFalse(True)
+        card =Card("Fireball")
+        self.assertNotEqual(card.name, "Thunder")
+        self.assertNotEqual(card.card_type, "heal")
+        self.assertNotEqual(card.power, 30)
+        self.assertNotEqual(card.cost, 2)
+
+    def test_valid_cursed_cards(self):
+        card=Card("Cursed Card")
+        self.assertEqual(card.name, "Cursed Card")
+        self.assertEqual(card.card_type, "cursed")
+        self.assertTrue(card.cursed)
+    
+    def test_valid_healing_cards(self):
+        card=Card("Heal")
+        self.assertEqual(card.name, "Heal")
+        self.assertEqual(card.card_type, "heal")
+        self.assertTrue(card.is_heal)
+
+
 
 
 if __name__ == '__main__':
